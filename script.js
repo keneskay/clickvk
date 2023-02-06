5f41bb83b135c93b721275c4863d76203e0c3bed
keneskay <98239713+keneskay@users.noreply.github.com>
2023-02-06 05:22:19 +0500

    Audio Mixer

--- script.js
+++ script.js
@@ -1,23 +1,20 @@
-let step = 0;  // 0 = Крестик, 1 = Нолик
+let volume = 0;
 
-function slot(a) {
-    let hod = document.getElementById('slot' + a).innerText;
-    if (hod != "X") {
-        if (hod != 'O') {
-            if (step == 0) {
-                document.getElementById('slot' + a).innerHTML = 'X';
-                step = 1
-            }
-            else {
-                step = 0
-                document.getElementById('slot' + a).innerHTML = 'O';
-            }
-        }
-        else {
-            alert("Здесь нельзя сходитЬ!")
-        }
-    }
-    else {
-        alert("Здесь нельзя сходитЬ!")
-    }
+snake = document.getElementsByClassName('volume-snake')[0];
+
+function PlusVolume() {
+  if (volume != 230) {
+    volume = volume + 23;
+    snake.style.height = volume;
+  }
 }
+
+function MinusVolume() {
+  if (volume != 0) {
+    volume = volume - 23;
+    snake.style.height = volume;
+  }
+}
+
+
+
